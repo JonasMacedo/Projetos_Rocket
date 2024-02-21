bc917248f847d836e1faf36eb463239dd2aae421
Jonas <jonasmdcarvalho@gmail.com>
2024-02-20 21:27:46 -0300

    Predefinindo campo Tags

--- ListaDados/src/app.tsx
+++ ListaDados/src/app.tsx
@@ -1,7 +1,28 @@
+import {Plus} from 'lucide-react'
 
 function App() {
   return (
-    <h1>Ola!</h1>
+    <div className="py-10 space-y-8">
+
+      <div>
+        <h1>Ola!</h1>
+        <p>Header</p>
+        <p>Tabs</p>
+
+      </div>    
+
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className='flex gap-3 items-center'>
+          <h1 className="text-xl font-bold">Tags</h1>
+          <button className='inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-2 py-1'>
+            <Plus className='size-3'/>        
+            Create new
+          </button>
+        </div>
+
+      </main>
+
+    </div>
   )
 }
 
