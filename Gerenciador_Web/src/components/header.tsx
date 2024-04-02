1798fc13da88c66c326b275bb056c65e4ce06c1b
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-04-02 15:46:29 -0300

    Estilizacao do Header

--- Gerenciador_Web/src/components/header.tsx
+++ Gerenciador_Web/src/components/header.tsx
@@ -1,7 +1,14 @@
+import nlwIcon from "../assets/nlw_icon.svg"
+
 export function Header(){
     return(
-        <div>
-            header
+        <div className="flex items-center gap-5 py-2">
+            <img src={nlwIcon}/>
+            
+            <nav className="flex items-center gap-5">
+                <a href="#" className="font-medium textsm text-zinc-300">Eventos</a>
+                <a href="#" className="font-medium textsm">Participantes</a>
+            </nav>
         </div>
     )
 }
