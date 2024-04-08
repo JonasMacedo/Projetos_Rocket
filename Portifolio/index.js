0442e0166f9e6bda7468728e9f666757037771b4
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-04-08 17:45:43 -0300

    Add efeitos com JavaScript

--- Portifolio/index.js
+++ Portifolio/index.js
@@ -1,1 +1,21 @@
-window.alert("Teste")
+
+const btnWantMore = document.querySelector('.wantMore');
+const projetoInativos = document.querySelectorAll('.project:not(active)') // para buscar todos os elementos inativos
+//const projetoInativos = document.querySelectorAll('.project')
+
+console.log(projetoInativos);
+
+btnWantMore.addEventListener('click', ()=>{
+    
+    projetoInativos.forEach(proj => {
+        // console.log(proj)
+        proj.classList.remove('project')
+        proj.classList.add('project_active')
+    })
+
+    btnWantMore.classList.remove('wantMore')
+    btnWantMore.classList.add('remover')
+
+})
+
+
