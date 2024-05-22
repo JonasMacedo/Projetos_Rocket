c818a370113f5231c020e5910a5bab43b89bbf51
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-05-22 09:03:28 -0300

    Criando Server basico com NodeJS

---
+++ NodeJS_Basico/Criando_Server_Basico/index.js
@@ -0,0 +1,17 @@
+
+// acesse o https://http.cat/ para verificar os status HTTP.
+
+const http = require('http') // biblioteca oriunda do NodeJS.
+const port = 3030 // definindo a porta que sera verificada.
+
+
+http.createServer((req,resp)=>{
+    
+    console.log('Recebendo uma requisicao')
+
+    resp.writeHead(200,{
+        'Content-Type': 'text/plain'
+    })
+    resp.write('Basico de NodeJS\n')
+    resp.end()
+}).listen(port)
---
+++ NodeJS_Basico/Criando_Server_Basico/readme.txt
@@ -0,0 +1,10 @@
+Informacoes adicionais.
+
+Para verificar os status HTTP acesse o https://http.cat
+
+Metodo createServer ira criar um servidor HTTP, utilizando o NodeJS, que obrigatoriamente 
+possui 2 parametros REQUISICAO e RESPOSTA.
+
+Para executar um servidor em node, basta acessar a pasta pelo CMD e executar o comando: 
+
+- node "aquivo.JS"
