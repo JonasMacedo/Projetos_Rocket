a91ea19f22117e7eca6db3abb029135b44420287
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-05-23 17:14:08 -0300

    Baisco de modularizacao

---
+++ NodeJS_Basico/Modularizacao/rotas.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const rotas = express.Router();
+
+let cursosInfo = [
+    {'curso':'react', 'info':'Curso de React'},
+    {'curso':'node', 'info':'Curso de NodeJS'},
+    {'curso':'css', 'info':'Curso de CSS'},
+    {'curso':'python', 'info':'Curso de Python'}
+]
+
+rotas.get('/',(req,res)=>{
+    res.json({ola:'Seja bem vindo'})
+})
+
+rotas.get('/:cursoId',(req,res)=>{
+    
+    const curso = req.params.cursoId
+    const infCurso = cursosInfo.find(i=>i.curso == curso)
+
+    if(!infCurso){
+        res.status(404).json({erro:'Curso não encontrado'})
+    }else{
+        res.status(200).json({infCurso})
+    }
+})
+
+module.exports = rotas
