71845a24eed0b8631805ce45d0c21f872e9e3f9c
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-05-27 11:34:42 -0300

    Add dados da conexao com banco

---
+++ NodeJS_Basico/Conexao_FireBird/db.js
@@ -0,0 +1,14 @@
+const dbOptions = { 
+    host : '127.0.0.1',
+    port : 3050,
+    database : 'C:\\ECOSIS\\DADOS\\PAF_RODRIGUES.eco', // caminho do banco na maquina.
+    user : 'SYSDBA',
+    password : 'masterkey',
+    lowercase_keys : false, // set to true to lowercase keys
+    role : null,            // default
+    pageSize : 4096,        // default when creating database
+    pageSize : 4096,        // default when creating database
+    retryConnectionInterval : 1000, // reconnect interval in case of connection drop
+    blobAsText : false, // set to true to get blob as text, only affects blob subtype 1
+    encoding : 'UTF-8', // default encoding for connection is UTF-8
+};
