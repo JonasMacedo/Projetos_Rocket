c240ed7c93ee218b2ccb78562cb50a9a9ac8bf58
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-05-28 18:09:22 -0300

    Add funcao post

--- NodeJS_Basico/Conexao_FireBird/index.js
+++ NodeJS_Basico/Conexao_FireBird/index.js
@@ -31,5 +31,34 @@ app.get("/lista", function(req,res){
     })
 })
 
+app.post("/add", function(req,res){
+
+    let filtro = []
+    let sql = 'update testprodutogeral set descricao = ?, codigo = ? where codigo > 0 '
+    let valorCodigo = Number.parseInt(req.query.codigo)
+    
+    console.log(req.body.descricao, req.body.codigo)
+
+     // http://localhost:3033/add?descricao=Andorinha&codigo=1055
+    
+    if (req.query.descricao && valorCodigo != 0 ){ // verifica se foi informado um parametro na rota HTTP.
+        valorCodigo = Number.parseInt(req.query.codigo)
+        // return console.log(valorCodigo, typeof valorCodigo)
+        
+        sql+= "and descricao like ?"      
+            
+        filtro.push("%"+req.query.descricao+"%") // adicionando para o filtro.
+        filtro.push(valorCodigo) 
+    }
+    
+    findItems(sql, filtro, function(error, result){
+        if (error) {
+            res.status(500).json(error)
+        }else{
+            res.status(200).json(result)
+        }
+    })
+})
+
 
 app.listen(3033,()=>{console.log('Servidor ativo\nDesativar: CTRL+C')})
