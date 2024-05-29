580c04d1de9176b456d6187927c31ff19203d906
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-05-29 17:25:43 -0300

    Add rota de pedido

--- NodeJS_Basico/Conexao_FireBird/index.js
+++ NodeJS_Basico/Conexao_FireBird/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { executeQuery } from "./db.js" // importando a funcao de busca.
+import { executeQuery, fbOptions, Firebird } from "./db.js" // importando a funcao de busca.
 
 const app = express()
 
@@ -33,7 +33,7 @@ app.get("/lista", function(req,res){
 
 app.post("/add", function(req,res){
 
-    let sql = 'INSERT INTO testprodutogeral(descricao, codigo) VALUES(?,?) RETURN CODIGO'
+    let sql = 'INSERT INTO testprodutogeral(descricao, codigo) VALUES(?,?) RETURNING CODIGO'
          
     console.log(req.body.descricao, req.body.codigo)
 
@@ -44,6 +44,35 @@ app.post("/add", function(req,res){
         res.status(200).json({codigo: result.codigo})
         }
     })
+})
+
+app.post("/pedidos", function(req,res){
+
+    // Ira trabalhar com 2 tabelas, devera manter a integridade de ambas, precisara usar uma TRANSACAO.
+    Firebird.attach(fbOptions, function(err, db) {
+        
+        if(err){
+            return res.status(500).json(err)
+        }
+        
+        // Ira realizar a transacao, caso consiga ira grava a operacao no banco, caso ñ consiga ira fazer o rollback desfazendo tudo.
+        db.transaction(Firebird.ISOLATION_READ_COMMITTED, function(err,transaction) {
+            
+            if(err){
+                return res.status(500).json(err)
+            }
+            
+            try { // ira tentar realizar a operacao.
+                
+                let sql = "inset into tvenpedido(idcliente,idvenda) values(?,?) returning idpedido"
+
+            } catch (error) {
+                
+            }
+            
+        })
+    })
+    
 })
 
 
