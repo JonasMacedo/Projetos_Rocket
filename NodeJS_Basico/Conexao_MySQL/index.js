ae04895b0f142c81837001b70055ec0c8f5b2c2f
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-06-05 16:12:13 -0300

    RMV conexado com o FireBird

--- NodeJS_Basico/Conexao_MySQL/index.js
+++ NodeJS_Basico/Conexao_MySQL/index.js
@@ -1,7 +1,6 @@
 import express from "express"
 import cors from "cors"
-import {dbOptions} from "./db.js"
-import firebird from "node-firebird"
+
 
 const app = express()
 
@@ -14,29 +13,7 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/produtos",(req,res)=>{
-    
-    firebird.attach(dbOptions, function(err, db) {
         
-        if (err){
-            return res.status(500).json(err)
-        }
-        
-        res.status(200).send('Listando Produtos:')
-        
-        // Realizando a consulta no banco do FireBird
-        db.query('SELECT codigo, descricao FROM testprodutogeral', function(err, result) {
-            // IMPORTANT: close the connection
-            db.detach();
-            
-            if (err){
-                return res.status(500).json(err)
-            }else{
-                return res.status(200).json(result)
-            }
-            
-        });
-    
-    });
 
 })
 
