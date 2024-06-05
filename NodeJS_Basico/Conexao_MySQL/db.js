c29cc83bae1b1018a6f9248a03cdda6f2e58ce5f
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-06-05 18:04:30 -0300

    UPD porta do servidor

--- NodeJS_Basico/Conexao_MySQL/db.js
+++ NodeJS_Basico/Conexao_MySQL/db.js
@@ -1,19 +1,4 @@
 
-const dbOptions = { 
-    host : '127.0.0.1',
-    port : 3050,
-    database : 'C:\\ECOSIS\\DADOS\\PAF_RODRIGUES.eco', // caminho do banco na maquina.
-    user : 'SYSDBA',
-    password : 'masterkey',
-    lowercase_keys : false, // set to true to lowercase keys
-    role : null,            // default
-    pageSize : 4096,        // default when creating database
-    pageSize : 4096,        // default when creating database
-    retryConnectionInterval : 1000, // reconnect interval in case of connection drop
-    blobAsText : false, // set to true to get blob as text, only affects blob subtype 1
-    encoding : 'UTF-8', // default encoding for connection is UTF-8
-};
-
 const connect = async ()=>{ // conectando com o banco MySQL
 
     if(global.conexao && global.conexao.state != 'disconected') { // verifica o status da conexao.
@@ -21,7 +6,7 @@ const connect = async ()=>{ // conectando com o banco MySQL
     }
 
     const mysql = require('mysql2/promise')
-    const conecta = mysql.createConnection("mysql://root:123456789@localhost:3306")
+    const conecta = mysql.createConnection("mysql://root:123456789@localhost:3306/estudo")
     console.log('conectou ao banco')
 
     global.conexao=conecta
@@ -54,4 +39,5 @@ const deletClient = async(id)=>{
     await  con.query(sql, valores)
 }
 
-export{dbOptions,connect,allClients, addClient, updateClient, deletClient}
+conect()
+export{connect, allClients, addClient, updateClient, deletClient}
