5a751e4afc7266f41845e957046237f5f6f79610
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-06-25 16:38:14 -0300

    Add conexao com o MongoDB

--- NodeJS_Basico/EstudoMongoDB/index.js
+++ NodeJS_Basico/EstudoMongoDB/index.js
@@ -1,12 +1,22 @@
-import mongodb, {MongoClient} from 'mongodb'
+
+
 import express from 'express'
+import mongodb, {MongoClient} from 'mongodb'
 
 const app = express()
 const mongoAdmin = mongodb
-console.log(mongoAdmin)
-
-const urlMongo = "mongodb+srv://jonasmdcarvalho:<password>@estudonodejs.jg2bdpr.mongodb.net/?retryWrites=true&w=majority&appName=EstudoNodeJs"
 
 
+const urlMongo = "mongodb+srv://jonasmdcarvalho:Nodejs@estudonodejs.jg2bdpr.mongodb.net/?retryWrites=true&w=majority&appName=EstudoNodeJs"
+// const urlMongo = "mongodb%2Bsrv%3A%2F%2Fjonasmdcarvalho%3A%3CNodejs%3E%40estudonodejs.jg2bdpr.mongodb.net%2F%3FretryWrites%3Dtrue%26w%3Dmajority%26appName%3DEstudoNodeJs"
+const clientMongo = new MongoClient(urlMongo)
+const dbName = 'estudonodejs'
+
+async function connectMongo(){
+    await clientMongo.connect()
+    clientMongo.db("estudonodejs")
+    console.log('conectado ao MongoDB')
+}
+connectMongo()
 
 app.listen(3035, ()=>{console.log('Servidor ativo \nPara desativar Ctrl+C')})
