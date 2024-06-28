c09892ce612f349cc8a11db93b4e4ea222d75e00
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-06-28 12:06:18 -0300

    Add rotas Post e Get

--- NodeJS_Basico/EstudoMongoDB/index.js
+++ NodeJS_Basico/EstudoMongoDB/index.js
@@ -1,21 +1,45 @@
-
 import express from 'express'
 import {MongoClient} from 'mongodb'
-import {connectMongo} from './db'
+
+import mongoose from 'mongoose'
+import User from './Models/users.js'
 
 const app = express()
+app.use(express.json())
+
 const urlMongo = "mongodb+srv://jonasmdcarvalho:Nodejs@estudonodejs.jg2bdpr.mongodb.net/?retryWrites=true&w=majority&appName=EstudoNodeJs"
 const clientMongo = new MongoClient(urlMongo)
+
+app.post('/adduser', async (req, res)=>{
+
+    let user = req.body
+
+    let newUser = await User.create(user)
+    return res.json(newUser)
+
+})
+
+app.get('/getusers', async (req, res)=>{
+
+    let allUser = await User.find()
+    return res.json(allUser)
+
+})
 
 /*
 async function connectMongo(){ //Conectado direto pelo MONGO.
     await clientMongo.connect()
     clientMongo.db("estudonodejs")
     console.log('conectado ao MongoDB')
-} 
+    } 
+    connectMongo()
 */
+   
+mongoose.connect(urlMongo)
+.then(()=>{
+    console.log('conectado ao MongoDB')       
+}).catch(()=>{
+    console.log('Falhou a conexão com o MongoDB')           
+})
 
-connectMongo()
-// connectMongo()
-
-app.listen(3035, ()=>{console.log('Servidor ativo \nPara desativar Ctrl+C')})
+app.listen(3035, ()=>{console.log('Servidor ativo!!🚀 \nPara desativar Ctrl+C')})
