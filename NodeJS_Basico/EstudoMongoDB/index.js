c89bf8d66687a7538870e05d4ae4ac18c541523b
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-07-01 15:26:17 -0300

    Adj modularizacao dos metodos Get e Post

--- NodeJS_Basico/EstudoMongoDB/index.js
+++ NodeJS_Basico/EstudoMongoDB/index.js
@@ -1,45 +1,25 @@
 import express from 'express'
-import {MongoClient} from 'mongodb'
 
-import mongoose from 'mongoose'
-import User from './Models/users.js'
+import { addUser, allUsers } from './db.js' 
 
 const app = express()
 app.use(express.json())
 
-const urlMongo = "mongodb+srv://jonasmdcarvalho:Nodejs@estudonodejs.jg2bdpr.mongodb.net/?retryWrites=true&w=majority&appName=EstudoNodeJs"
-const clientMongo = new MongoClient(urlMongo)
-
 app.post('/adduser', async (req, res)=>{
 
-    let user = req.body
+    let userDados = req.body
+    let user = await addUser(userDados)
 
-    let newUser = await User.create(user)
-    return res.status(201).json(newUser)
+    return res.status(201).json(user)
 
 })
 
 app.get('/getusers', async (req, res)=>{
 
-    let allUser = await User.find()
-    return res.status(200).json(allUser)
+    let all = await allUsers()
+    return res.status(200).json(all)
 
 })
 
-/*
-async function connectMongo(){ //Conectado direto pelo MONGO.
-    await clientMongo.connect()
-    clientMongo.db("estudonodejs")
-    console.log('conectado ao MongoDB')
-    } 
-    connectMongo()
-*/
-   
-mongoose.connect(urlMongo)
-.then(()=>{
-    console.log('conectado ao MongoDB')       
-}).catch(()=>{
-    console.log('Falhou a conexão com o MongoDB')           
-})
 
 app.listen(3035, ()=>{console.log('Servidor ativo!!🚀 \nPara desativar Ctrl+C')})
