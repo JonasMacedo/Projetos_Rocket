f6a2fd79470881cf062fb396e9a854b1caf92643
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-07-18 15:26:35 -0300

    Rmv metodo DeleteUser

--- NodeJS_Basico/EstudoMongoDB/db.js
+++ NodeJS_Basico/EstudoMongoDB/db.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import User from "./Models/users.js";
 
 async function connectMongo(){
     const urlMongo = "mongodb+srv://jonasmdcarvalho:Nodejs@estudonodejs.jg2bdpr.mongodb.net/?retryWrites=true&w=majority&appName=EstudoNodeJs"
@@ -7,29 +6,6 @@ async function connectMongo(){
     .then(()=>{console.log("conectou ao MongoDB @")})
     .catch(()=>{("Falha na conexão com o MongoDB!!")})
     
-}
-
-
-const deleteUser = async(id)=>{
-
-    let connect = await connectMongo()
-    
-    try {
-        let user = await User.findByIdAndDelete(id)
-        
-        if (!user) {
-            return user.message    
-        }    
-        
-        let users = await User.find()
-        return users
-
-    } catch (error) {
-        return error
-    }
-
-
-
 }
 
 export{connectMongo}
