31655c9ab6707728d071b35d068f743d439427c2
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-07-18 15:42:05 -0300

    Add metodo de rotas

--- NodeJS_Basico/EstudoMongoDB/index.js
+++ NodeJS_Basico/EstudoMongoDB/index.js
@@ -1,48 +1,12 @@
 import express from 'express'
+import routerUser from './Routes/user.routes.js'
 
-import { addUser, allUsers, deleteUser, findUser, upDateUser } from './db.js' 
-
+// const router = express.Router()
 const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false})) // Para que o receba formularios por URL
 
-app.post('/adduser', async (req, res)=>{
-
-    let userDados = req.body
-    let user = await addUser(userDados)
-
-    return res.json(user)
-
-})
-
-app.get('/getallusers', async (req, res)=>{
-    
-    let all = await allUsers()
-    return res.status(200).json(all)
-    
-})
-
-app.get('/getuser/:id', async (req, res)=>{
-    
-    let {id} = req.params
-    // console.log('Id informado: ', id)
-    
-    let user = await findUser(id)
-    return res.status(200).json(user)
-    
-})
-
-app.put('/attuser', async(req,res)=>{
-    
-    let user = await upDateUser(req.body)
-    return res.json(user)
-})
-
-app.delete('/deleteuser/:id', async(req,res)=>{
-    
-    let {id} = req.params
-    let user = await deleteUser(id)
-    return res.status(200).json(user)
-})
+app.use('/api', routerUser)
 
 app.listen(3035, ()=>{console.log('Servidor ativo!!🚀 \nPara desativar Ctrl+C')})
