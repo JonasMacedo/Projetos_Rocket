803e5102475e7f68208b2aaf923523b699854189
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-07-23 17:21:58 -0300

    importanto e utilizando o FastiFy

--- NodeJS_Basico/FastyFy/server.js
+++ NodeJS_Basico/FastyFy/server.js
@@ -1,5 +1,13 @@
+
+/* Sem o FastiFy
 import {createServer} from 'node:http'
-
 const server = createServer()
-
 server.listen(3030, console.log(console.log('Servidor ativo!!🚀 \nPara desativar Ctrl+C')))
+*/
+
+import { fastify } from "fastify"; 
+
+const server = fastify()
+server.listen({
+    port:3035
+},console.log('Servidor ativo!!🚀 \nPara desativar Ctrl+C'))
