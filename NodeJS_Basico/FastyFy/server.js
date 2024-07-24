f68e9bb63860050e2227ccf9b983af926c5a798c
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-07-24 17:32:34 -0300

    Add rota PUT

--- NodeJS_Basico/FastyFy/server.js
+++ NodeJS_Basico/FastyFy/server.js
@@ -25,6 +25,20 @@ server.post('/videos',(req, res)=>{
     return res.status(201).send()
 })
 
+server.put('/video/:id',(req, res)=>{
+    
+    let {id} = req.params
+    let {title, description, duration} = req.body
+
+    database.update(id, {
+        title,
+        description,
+        duration,
+    })
+
+    return res.status(204).send()
+})
+
 server.get('/allvideos',()=>{
     let videos = database.list()
     console.log('Get: ',videos)
