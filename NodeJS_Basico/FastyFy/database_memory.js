bb89c1de75d30319cc2b96eb6902b9bd573d8dbd
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-07-24 16:12:03 -0300

    concluindo metodos do CRUD

--- NodeJS_Basico/FastyFy/database_memory.js
+++ NodeJS_Basico/FastyFy/database_memory.js
@@ -4,11 +4,21 @@ export class databaseMoemory {
     #videos = new Map()
     
     create(video){
-        const videoId = randomUUID()
+        const videoId = randomUUID() // funcao do NodeJS para gerar Id unico.
         this.#videos.set(videoId, video)
     }
 
     update(id,video){
         this.#videos.set(id, video)
     }
+    
+    delete(id){
+        this.#videos.delete(id)
+    }
+    
+    list(){
+        this.#videos.values()
+    }
+    
+    
 }
