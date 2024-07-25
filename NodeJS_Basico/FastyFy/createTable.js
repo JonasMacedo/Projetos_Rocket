f13c46691c163c946a289ad70650a6d9097ba9e2
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-07-25 16:53:55 -0300

    d metodo de Drop da tabelaão

--- NodeJS_Basico/FastyFy/createTable.js
+++ NodeJS_Basico/FastyFy/createTable.js
@@ -1,7 +1,14 @@
 import {sql} from "./db.js";
 
+/* Descomentar APENAS se for necessario deletar a tabela toda!
+sql`DROP TABLE IF EXISTS VIDEOS`.then(()=>{
+    console.log('Removida a tabela')
+})
+*/
+
 sql`
 CREATE TABLE videos (
+    id Text PRIMARY KEY,
     title Text,
     description Text,
     duration Integer
