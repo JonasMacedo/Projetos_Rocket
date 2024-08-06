4e8ca83dadebfab792364a0cf34246834292f48a
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-08-06 15:03:04 -0300

    Add registro de rota no Server

--- NodeJS_Basico/Prisma/src/server.ts
+++ NodeJS_Basico/Prisma/src/server.ts
@@ -1,10 +1,15 @@
 import fastify,{FastifyInstance} from "fastify";
 import { userRoutes } from "./routes/user_routes";
+import { ContactRoutes } from "./routes/contacts_routes";
 
 const server : FastifyInstance = fastify()
 
 server.register(userRoutes,{
     prefix: '/users'
+})
+
+server.register(ContactRoutes,{
+    prefix: '/contacts'
 })
 
 server.listen({port: 3035}, ()=>{console.log('Servidor ativo!!🚀 \nPara desativar Ctrl+C')})
