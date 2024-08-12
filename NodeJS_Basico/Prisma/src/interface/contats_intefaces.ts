da49a6a2e1d01c200727553eb4d8488e85fe1f65
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-08-12 16:53:32 -0300

    Adj nos metodos crate contacts

--- NodeJS_Basico/Prisma/src/interface/contats_intefaces.ts
+++ NodeJS_Basico/Prisma/src/interface/contats_intefaces.ts
@@ -14,7 +14,15 @@ export interface ContactCreate {
     userEmail: string,
 }
 
+export interface ContactCreateData {
+    id: string,
+    name: string,
+    email: string,
+    phone: string, 
+    userId: string,
+}
+
 export interface ContactRepository{
-    create(data:ContactCreate):  Promise<Contact>
+    create(data:ContactCreateData):  Promise<Contact>
     findByEmailOrPhone(email: string, phone: string): Promise<Contact | null>
 }
--- NodeJS_Basico/Prisma/src/repositories/contacts_repository.ts
+++ NodeJS_Basico/Prisma/src/repositories/contacts_repository.ts
@@ -1,9 +1,20 @@
 import { Prisma } from "@prisma/client";
-import { Contact, ContactCreate, ContactRepository } from "../interface/contats_intefaces";
+import { Contact, ContactCreate, ContactCreateData, ContactRepository } from "../interface/contats_intefaces";
 import { prisma } from "../database/prisma_client";
 
 class ContactsRepositoryPrisma implements ContactRepository {
-    create(data: ContactCreate): Promise<Contact>{}
+    
+    async create(data: ContactCreateData): Promise<Contact>{
+        const result = await prisma.contacts.create({
+            data:{ 
+                name: data.name,
+                email: data.email,
+                phone: data.phone,
+                userId: data.userId, 
+            }
+        })
+        return result
+    }
     
     async findByEmailOrPhone(email:string, phone: string): Promise<Contact | null>{
         const result = await prisma.contacts.findFirst({
