31fab0c32f15a558c1631ff6d601516416d921c8
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-09-24 11:02:55 -0300

    Add arquivos referente ao DB

---
+++ NodeJS_Basico/Conexao_MySQL/src/database/db.js
@@ -0,0 +1,62 @@
+
+const connect = async ()=>{ // conectando com o banco MySQL
+    
+    if(global.conexao && global.conexao.state != 'disconected') { // verifica o status da conexao.
+        return global.conexao
+    }
+        
+    const mysql = import ('mysql2/promise')
+    const conecta = (await mysql).createConnection("mysql://root:jonas@123@localhost:3306/estudo_node")
+    console.log('conectou ao banco')
+
+    global.conexao=conecta
+    return conecta
+}
+
+const allClients = async()=>{
+    console.log('Metodo de busca clientes')
+    const con = await connect()
+    const [lines] = await con.query('select * from cliente;')
+    return  lines
+}
+
+const addClient = async(cliente)=>{
+    console.log(cliente)
+    const con = await connect()
+    
+    const sql = 'insert into cliente (nome,idade,sobreNome) values(?,?,?)'
+    const valores = [cliente[0], cliente[1], cliente[2]]
+    await  con.query(sql, valores)
+}
+
+const updateClient = async(id, cliente)=>{
+    const con = await connect()
+    const sql = 'update cliente set nome=?, idade=?, sobrenome=? where idCliente=?'
+    
+    // Os valores precisam estar na mesma ordem de query.
+    const valores = [cliente[0], cliente[1], cliente[2], id]
+    await  con.query(sql, valores)
+}
+
+const deletClient = async(id)=>{
+    const con = await connect()
+    const sql = 'delete from cliente where idCliente=?'
+    const valores = [id]
+    await con.query(sql, valores)
+}
+
+connect() // Ele mesmo já executa a conexão com o banco.
+
+export{connect, allClients, addClient, updateClient, deletClient}
+
+/* 
+create schema Estudo_node;
+use estudo_node
+
+create table cliente(
+	idCliente int primary key not null auto_increment,
+    nome varchar(10),
+    idade int 
+);
+
+*/
