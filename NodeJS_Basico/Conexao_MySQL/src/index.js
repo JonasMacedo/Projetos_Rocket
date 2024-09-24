755d54b6ba62a818d5992eeda65c5681b610e759
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-09-24 15:00:12 -0300

    Add rota no index

--- NodeJS_Basico/Conexao_MySQL/src/index.js
+++ NodeJS_Basico/Conexao_MySQL/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
-import {addClient, allClients, deletClient, updateClient} from './db.js'
+
+import Route from './routes/routes.js'
 
 const app = express()
 
@@ -8,55 +9,7 @@ app.use(express.json())
 
 app.use(cors())
 
-app.get("/",(req,res)=>{
-    res.status(200).send('Pagina Home')
-})
-
-app.get("/allClients", async (req,res)=>{
-    
-    const clientes = await allClients()
-    
-    console.log(clientes)
-    res.json(clientes)
-    
-})
-
-app.post("/addClient", async (req,res)=>{
-    
-    var client = [req.body.nome, req.body.idade, req.body.sobrenome]
-    
-    //console.log(client)
-    
-    const newCliente = await addClient(client)
-    res.json(newCliente)
-    
-})
-
-app.put("/updateClient", async (req,res)=>{
-    
-    let iD = req.body.id
-    let client = [req.body.nome, req.body.idade, req.body.sobrenome]
-
-    if ( isNaN(iD)) {
-        res.status(400).json(`o ID ${iD} é do tipo: ${typeof iD}, e o cliente é do tipo ${typeof client}`)
-    }else{
-        await updateClient(iD, client)
-        res.json(`Cliente ${iD} atualizado`);
-    }
-
-})
-
-app.delete("/deletClient", async (req,res)=>{
-    let iD = parseInt(req.body.id)
-    
-    if ( isNaN(iD)) {
-        res.status(400).json(`o ID ${iD} é do tipo: ${typeof iD}`)
-    }else{
-        await deletClient(iD)
-        res.json(`Cliente ${iD} Removido`);
-    }
-    
-})
+app.use('/', Route)
 
 app.listen(3035,()=>{console.log("Servidor MySQL ativo\nPara desativar: Ctrl+C")})
 
