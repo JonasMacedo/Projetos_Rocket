0a723a8542e50205a53f79e1a49638b040767d63
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-11-21 16:00:26 -0300

    Add verificao de user/password

---
+++ NodeJS_Basico/Test_End2End/api.test.js
@@ -0,0 +1,33 @@
+import {describe, before, after, it } from "node:test"
+import {deepStrictEqual, strictEqual} from 'node:assert'
+
+const BASE_URL = 'http://localhost:3035'
+
+describe('API WorkFlow', ()=>{
+    let _server = {}
+
+    before(async ()=>{
+        //import dinamico
+        _server = (await import('./api.js')).app
+        await new Promise(res => _server.once('listening', res))
+    })
+
+    after(done => _server.close(done))
+
+    it('Recebido usuario e senha nao autorizado', async()=>{
+        const data = {
+            user: 'Jonas',
+            password: ' ',
+        }
+        
+        const request = await fetch(`${BASE_URL}/login`, {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        
+        strictEqual(request.status, 401)
+
+        const response = await request.json()
+        deepStrictEqual(response, {error:'usuario invalido!!'})
+    })
+})
