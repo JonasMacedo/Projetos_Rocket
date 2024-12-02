960c7ec912700bf447ce347ae25a06622bfd2e71
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-12-02 16:56:12 -0300

    Ajuste no metodo CreateUsers

--- NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/Controllers/Users.Controller.js
+++ NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/Controllers/Users.Controller.js
@@ -1,41 +1,63 @@
 import { PrismaClient} from '@prisma/client'
 import bcrypt from 'bcrypt'
 
+import { connectMongo } from '../Database/db.js'
+
 const prisma = new PrismaClient() 
 
 const createUsers = async (req, res) => {
+   
+   let connect = await connectMongo()
+   let user = req.body
+   console.log('Rota ativa, Creater:\n',req.body)
+   
+   try {       
+       
+    // let findUser = await user.findUnique({where: user.id})
     
-    let user = req.body
-    console.log('Rota ativa, parametros:\n',req.body)
+    let findUser = await prisma.users.findUnique({where: {email:user.name}})
+    console.log("FindUser: ", findUser)
 
-    try {
+    if (!findUser) {
         
-        let findUser = await user.findUnique({where: user.id})
-        
-        if (!findUser) {
-            
-            let cryptPassword = await bcrypt.genSalt(10) // gerando cryptografia sobre a senha.
-            let passwordHash = await bcrypt.hash(user.password, cryptPassword)
-            user.password = passwordHash
+        let cryptPassword = await bcrypt.genSalt(10) // gerando cryptografia sobre a senha.
+        let passwordHash = await bcrypt.hash(user.password, cryptPassword)
+        user.password = passwordHash
 
-            let newUser = await prisma.users.create(user)
-            return res.status(200).json(newUser)            
-        }
+        let newUser = await prisma.users.create({
+            data:{
+            email: user.email,
+            name: user.name,
+            age: user.age,
+            password: passwordHash,
+            },
+        })
 
+        return res.status(200).json(newUser)            
 
-    } catch (error) {
-        return res.status(500).json(error)
     }
+   } catch (error) {
+        return res.status(500).json(error)
+   }
     
 }
 
 const loginUser = async (req, res)=>{
     
+    console.log("rota login: \n", req.body)
+    let userInfo = req.body
+    
     try {
-
-        let userInfo = req.body
-        let findUser = await prisma.users.findUnique({where: userInfo.id})
         
+        let findUser = await prisma.users.findOne({email: userInfo.email})
+        console.log("rota login: \n", findUser)
+        
+        if (!findUser) {
+            return res.status(400).json({message: 'Usuario não encontrado'})
+        }
+
+        return res.status(200).json(findUser)
+
     } catch (error) {
         return res.status(500).json(error)        
     }
