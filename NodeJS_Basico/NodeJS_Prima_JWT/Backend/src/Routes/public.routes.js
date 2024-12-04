48b63c3b393f13d7da441899dcc0c4e296795993
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-12-04 10:15:16 -0300

    Remove rota de teste

--- NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/Routes/public.routes.js
+++ NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/Routes/public.routes.js
@@ -1,14 +1,10 @@
 import express from 'express'
-import { createUsers } from '../Controllers/Users.Controller.js'
+import { createUsers, loginUser } from '../Controllers/Users.Controller.js'
 
 const appRouter = express.Router()
 
 appRouter.post('/createuser', createUsers)
 
-appRouter.get('/user', (req, res)=>{
-    console.log('Rota Get')
-    return res.status(200).json({message: 'Rota ativa!'})
-})
-
+appRouter.post('/login', loginUser)
 
 export default appRouter
