dbd268839fe322b42dc5e38f71b9463fb9be63f5
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-12-13 17:57:02 -0300

    Add rota privada

--- NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/server.js
+++ NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import publicRoutes from './Routes/public.routes.js'
+import privateRoute from './Routes/private.routes.js'
 
 const app = express()
 
 app.use(express.json())
 app.use('/api', publicRoutes)
+app.use('/', privateRoute)
 
 app.listen(3035, ()=> console.log("Servidor Ativo!"))
 
