ab3e1ce620a90e9a5496bbbe85a81ee6a1f9016e
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-12-16 10:29:37 -0300

    Add listagem de usuarios na rota publica

--- NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/Routes/public.routes.js
+++ NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/Routes/public.routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createUsers, loginUser } from '../Controllers/Users.Controller.js'
+import { createUsers, listusers, loginUser } from '../Controllers/Users.Controller.js'
 
 const appRouter = express.Router()
 
 appRouter.post('/createuser', createUsers)
 
 appRouter.post('/login', loginUser)
+
+appRouter.get('/listusers', listusers)
 
 export default appRouter
