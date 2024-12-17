6c21d68ca9f00d96c9adc1adc2b29c87043b613a
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-12-17 18:12:27 -0300

    add validacao de token e decodificacao

--- NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/Middlewares/auth.js
+++ NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/Middlewares/auth.js
@@ -4,6 +4,26 @@ const JWT_SECRET = process.env.JWT_SECRET
 
 const auth = (req, res, next) =>{ // metodo de middlewares do NextJs.
 
-    
+    const token = req.headers.authorization
+    console.log("Token puro: ",token)
 
+    if(!token){
+        res.status(401).json({messagem: "Acesso negado"})
+    }
+    
+    const decoded = token.replace('Bearer ','') 
+    console.log("token decodificado: ", decoded)
+    
+    //jwt.verify(token.replace('Bearer ',''), jwt.JWT_SECRET)
+    
+    // try {
+
+    // } catch (error) {
+    //     return res.status(401).json({messagem:"Token Invalido."})
+    // }
+
+    next()
 } 
+
+
+export default auth
