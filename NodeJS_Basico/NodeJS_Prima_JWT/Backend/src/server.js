3eb16202b63edbf7b435205c798432f69f87d0c3
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-12-17 18:14:10 -0300

    add middleware a rota

--- NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/server.js
+++ NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
+
 import publicRoutes from './Routes/public.routes.js'
 import privateRoute from './Routes/private.routes.js'
+import auth from '../src/Middlewares/auth.js'
 
 const app = express()
 
 app.use(express.json())
 app.use('/api', publicRoutes)
-app.use('/', privateRoute)
+app.use('/', auth, privateRoute)
 
 app.listen(3035, ()=> console.log("Servidor Ativo!"))
 
