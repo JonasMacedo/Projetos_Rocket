fb9e9f632938877b167032b6080f3098e0f8f560
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-12-27 12:00:58 -0300

    Add Cors para comunicacao externa com BackEnd

--- NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/server.js
+++ NodeJS_Basico/NodeJS_Prima_JWT/Backend/src/server.js
@@ -3,10 +3,13 @@ import express from 'express'
 import publicRoutes from './Routes/public.routes.js'
 import privateRoute from './Routes/private.routes.js'
 import auth from '../src/Middlewares/auth.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors()) // habilita comunicacao externa.
+
 app.use('/api', publicRoutes)
 app.use('/', auth, privateRoute)
 
