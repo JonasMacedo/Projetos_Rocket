c2eb3d2d6e431314c76069cc1e536daeb74d64b2
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-12-27 17:44:36 -0300

    Ajuste no recebimento de dados FrontEnd

--- NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/pages/Cadastro/index.jsx
+++ NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/pages/Cadastro/index.jsx
@@ -8,22 +8,27 @@ function Cadastro() {
     const nameRef = useRef() // Verificar o uso de React Hoock Form, mais eficaz que useRef.
     const emailRef = useRef()
     const passwordRef = useRef()
+    const ageRef = useRef()
+    const sexRef = useRef()
 
     async function handleSubmit(event) {
         event.preventDefault()
 
         try {
-            await api.post('/createuser', { 
+            await api.post('/api/createuser', { 
                 name: nameRef.current.value,
                 email: emailRef.current.value,
-                password: passwordRef.current.value
+                age: parseInt(ageRef.current.value),
+                sex: sexRef.current.value,
+                password: passwordRef.current.value,
             })
             alert("Usuario cadastrado com Sucesso!!")
         } catch (error) {
             alert("Erro ao cadastrar usuario")            
         }
-
-        console.log(nameRef.current.value, emailRef.current.value, passwordRef.current.value)
+        
+        // console.log(typeof ageRef.current.value)
+        // console.log(nameRef.current.value, emailRef.current.value, passwordRef.current.value)
     }
 
     return(
@@ -31,6 +36,8 @@ function Cadastro() {
             <h2 className=" text-2xl font-bold mb-6 text-center text-gray-800 " >Cadastro</h2>
             <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
                 <input type="text" placeholder="Nome" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"  ref={nameRef}/>
+                <input type="number" placeholder="Idade" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"  ref={ageRef}/>
+                <input type="text" placeholder="Sexo" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"  ref={sexRef}/>
                 <input type="email" placeholder="Email" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" ref={emailRef}/>
                 <input type="password" placeholder="Senha" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" ref={passwordRef}/>
                 <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-400  " >Cadastrar-se</button>
--- NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/services/api.js
+++ NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://localhost:3035',
+    baseURL: 'http://localhost:3035',
     // timeout: 1000,
     // headers: {'X-Custom-Header': 'foobar'}
 });
