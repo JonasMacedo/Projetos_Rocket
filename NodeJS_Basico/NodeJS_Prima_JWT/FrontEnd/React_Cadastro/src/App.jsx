76628dc4cf972e1184faf56c515a167d187361ce
Jonas Macedo <jonasmdcarvalho@gmail.com>
2024-12-30 12:02:17 -0300

    Add pagina Login

--- NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/App.jsx
+++ NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
 
 import Cadastro from "./pages/Cadastro/index.jsx"
+import Login from "./pages/Login/index.jsx"
 
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Cadastro/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>    
     </BrowserRouter>
   )
 }
 
-export default App
+export default App 
