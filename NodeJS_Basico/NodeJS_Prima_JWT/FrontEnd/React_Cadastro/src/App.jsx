61aec26c8cb0c9599fffd57490d6fc04c1eab066
Jonas Macedo <jonasmdcarvalho@gmail.com>
2025-01-08 18:00:20 -0300

    Add rota de Login

--- NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/App.jsx
+++ NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
 
 import Cadastro from "./pages/Cadastro/index.jsx"
 import Login from "./pages/Login/index.jsx"
-
+import ListUsers from "./pages/Lista/index.jsx"
 
 function App() {
 
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Cadastro/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/listusers" element={<ListUsers/>}/>
       </Routes>    
     </BrowserRouter>
   )
