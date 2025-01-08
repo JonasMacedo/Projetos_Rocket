061231909a823902d1202b124932b33ab428c4b6
Jonas Macedo <jonasmdcarvalho@gmail.com>
2025-01-08 18:01:50 -0300

    Estilizacao da pagina de Login

--- NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/pages/Lista/index.jsx
+++ NodeJS_Basico/NodeJS_Prima_JWT/FrontEnd/React_Cadastro/src/pages/Lista/index.jsx
@@ -23,14 +23,14 @@ function ListUsers() {
     },[])
 
     return(
-        <div>
-            <h2>Lista de usuarios</h2>
-            <ul>
+        <div className="max-w-2xl mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-md shadow-lg">
+            <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Lista de usuarios</h2>
+            <ul className="space-y-2">
                 { allUsers && allUsers.length > 0 && allUsers.map((user) =>(
-                    <li key={user.id}>
-                        <p>{user.name}</p>
-                        <p>{user.age}</p>
-                        <p>{user.email}</p>
+                    <li key={user.id} className="bg-gray-100 p-6 rounded-md">
+                        <p className="font-semibold" >Nome: {user.name}</p>
+                        <p className="font-semibold" >Idade: {user.age}</p>
+                        <p className="font-semibold" >Email: {user.email}</p>
                     </li>
                 ))}
             </ul>
