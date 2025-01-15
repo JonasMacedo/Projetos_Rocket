27174109cdf54ca1d5754fcaea4b58b1e12f2d3b
Jonas Macedo <jonasmdcarvalho@gmail.com>
2025-01-15 16:05:07 -0300

    Listando produtos da Amazom

--- GuiaPrecos/main.js
+++ GuiaPrecos/main.js
@@ -1,5 +1,6 @@
 
 const searchForm = document.querySelector('.search_form')
+const producstList = document.querySelector('.product_list')
 
 searchForm.addEventListener('submit', async function(event){
     event.preventDefault()
@@ -10,5 +11,19 @@ searchForm.addEventListener('submit', async function(event){
 
     const dados = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${inputValue}`)
     const products = (await dados.json()).results
-    console.log(products)
+    // console.log(products)
+    
+    displayItems(products)
 })
+
+function displayItems(products){
+    console.log(products)
+    producstList.innerHTML = products.map(prod => `
+            <div class="product_card">
+                <img src="${prod.thumbnail}" alt="${prod.title}">
+                <h3>"${prod.title}"</h3>
+                <p>"${prod.price.toLocaleString('pt-br', {style:"currency", currency:"BRL"})}"</p>
+                <p>"${prod.seller.nickname}"</p>
+            </div>
+        `).join('')
+}
