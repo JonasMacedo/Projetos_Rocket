817b49bb8e00293e2eff38bba4af9c4f464784e6
Jonas <jonasmdcarvalho@gmail.com>
2025-09-11 18:56:15 -0300

    Add efeito de fechamento do modal

--- TelasLogin/JanelaModal/main.js
+++ TelasLogin/JanelaModal/main.js
@@ -2,5 +2,13 @@
 function ativaModal(){
     const modal = document.getElementById('janela_modal')
     modal.classList.add('abrir_modal')
+
+    modal.addEventListener('click', (e)=>{ 
+        
+        // Verifica para realizar fechamento do modal. 
+        if(e.target.id == 'fechar' || e.target.id == 'janela_modal'){
+            modal.classList.remove('abrir_modal')
+        }
+    })
 }
 
