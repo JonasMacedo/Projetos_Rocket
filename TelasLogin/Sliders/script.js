37b34e23ae536cf875e4027e69f68e0ff7dbca90
Jonas <jonasmdcarvalho@gmail.com>
2025-09-19 23:51:43 -0300

    Add arquivo JavaScript

---
+++ TelasLogin/Sliders/script.js
@@ -0,0 +1,7 @@
+let btnNext = document.querySelector('.next')
+let btnBack = document.querySelector('.next')
+
+let container = document.querySelector('.main_container')
+let list = document.querySelector('.main_container .list_item')
+
+
