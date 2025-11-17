abb988288f15e7b522fb2d7dad3b9f901806f216
Jonas Macedo <jonasmdcarvalho@gmail.com>
2025-11-17 16:55:32 -0300

    Add Recurso de mensagens para validacoes.

--- Projetos_C/CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
+++ Projetos_C/CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Comunnication.Request;
+using CashFlow.Exception;
 using FluentValidation;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -7,10 +8,11 @@ public class RegisterExpenseValidator : AbstractValidator<RequestExpenseJson>
     public RegisterExpenseValidator()
     {
         // Validacao sem usar a dependencia do NuGet FluentValidator, ler mais a respeito na documentacao.
-        RuleFor(expense => expense.Title).NotEmpty().WithMessage("The title is required.");
-        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("The amount must be greater than zero.");
-        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Date cannot be for the future.");
-        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage("Payment type is not valid.");
+        RuleFor(expense => expense.Title).NotEmpty().WithMessage(ResourceErrorMessages.TITLE_REQUIRED);
+        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
+        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.DATE_EXPENSES);
+        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
 
     }
 }
+ 
