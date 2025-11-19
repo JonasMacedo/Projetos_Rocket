af9d960833c3488c5e4d8a89a4a354d7d5a302d1
Jonas Macedo <jonasmdcarvalho@gmail.com>
2025-11-19 15:23:40 -0300

    Add adptacao de Warnings para outros idiomas.

--- Projetos_C/CashFlow/src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
+++ Projetos_C/CashFlow/src/CashFlow.Exception/ExceptionsBase/CashFlowException.cs
@@ -1,6 +1,7 @@
 ﻿namespace CashFlow.Exception.ExceptionsBase;
 public abstract class CashFlowException : SystemException 
 {
-
-
+    // Por ser uma classe precisaria ser instanciada, para evitar esse comportamento, é definido como uma classe Abstrata.
+    
+    // Para ser uma classe de Excessoes sera necessario ela ter a heranca do SysTemException.
 }
