430f61d4ee9e4b880d49c62572a7bd103f89660f
Jonas Macedo <jonasmdcarvalho@gmail.com>
2025-11-25 16:21:42 -0300

    Add funcao de localizacao.

--- Projetos_C/CashFlow/src/CashFlow.Api/Middleware/CultureMiddleware.cs
+++ Projetos_C/CashFlow/src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -1,5 +1,33 @@
-﻿namespace CashFlow.Api.Middleware;
+﻿using System.Globalization;
+
+namespace CashFlow.Api.Middleware;
 
 public class CultureMiddleware
 {
+    private readonly RequestDelegate _next;
+    public CultureMiddleware(RequestDelegate next)
+    {   
+        _next = next;   
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault(); // Extraindo do Header a cultura.
+
+        var cultureInfo = new CultureInfo("en"); // Instanciando um cultura padrao.
+
+        if (string.IsNullOrWhiteSpace(culture)== false) {
+            
+            //Verifica se a culture contem informacao.
+
+            cultureInfo = new CultureInfo(culture);
+        }
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+
+    }
+
 }
